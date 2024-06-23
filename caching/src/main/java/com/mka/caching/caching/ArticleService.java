348cab9dbd53c50d8095e5316c366a55efa2efdc
khizar <mohammedkhizar501@gmail.com>
2024-06-23 19:37:38 +0530

    Added Caching simple logic

--- caching/src/main/java/com/mka/caching/caching/ArticleService.java
+++ caching/src/main/java/com/mka/caching/caching/ArticleService.java
@@ -1,16 +1,22 @@
 package com.mka.caching.caching;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
 	
+	@Cacheable("articles")
 	public void fetchArticle(Integer articaleID) {
 		System.out.println("fetching Article: "+ articaleID);
 	}
+	@CachePut("articles")
 	public void updateArticle(Article articale) {
-		System.out.println("Updated  Article: "+ articale);
+		System.out.println("Updated  Article: "+ articale.getId());
 	}
+	@CacheEvict("articles")
 	public void deleteArticle(Integer articaleID) {
 		System.out.println("Deleted  Article: "+ articaleID);
 	}
--- caching/src/main/java/com/mka/caching/caching/CachingApplication.java
+++ caching/src/main/java/com/mka/caching/caching/CachingApplication.java
@@ -2,8 +2,11 @@ package com.mka.caching.caching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CachingApplication {
 
 	public static void main(String[] args) {
